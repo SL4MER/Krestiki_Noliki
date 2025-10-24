1b612ce6bcab9efa62a9b19490c936e02789aa4a
SL4MER <nekid2007@gmail.com>
2025-10-24 15:23:10 +0300

    сделал проверку ввода и закомитил

--- Krestiki_Noliki/Krestiki_Noliki.cpp
+++ Krestiki_Noliki/Krestiki_Noliki.cpp
@@ -2,7 +2,12 @@
 
 using namespace std;
 
-
+int Inputcorrectness(char a, int* i) {
+	if (a == 'x' || a == '0') {
+		cout << "\nПРЕДУПРЕЖДЕНИЕ: Данное поле уже занято, выберите другое \n";
+		return (*i)--;
+	}
+}
 
 void PrintArr(char a[3][3]) {
 	for (int i = 0; i < 3; i++) {
@@ -28,10 +33,11 @@ int main() {
 		cin >> x;
 		cout << "Введите номер столбца: ";
 		cin >> y;
+		Inputcorrectness(a[x - 1][y - 1], &i);
 		cout << endl;
 		a[x - 1][y - 1] = (i % 2 == 0) ? X : O;
 		PrintArr(a);
-
+	}
 
 
 	return 0;
